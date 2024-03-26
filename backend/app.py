ff60abc402969c1e15b059e248a264c495542724
Marco Diaz <marcodiazmoore@proton.me>
2024-03-26 14:31:32 -0400

    Initial commit of get API routing as well as added instructions for running the flask server

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,8 @@
-# Server can be ran with command: flask --app app run
+# The flask server can be ran with the following commands:
+#
+# .venv\Scripts\activate
+# flask --app app run
+#
 
 from flask import Flask
 from markupsafe import escape # escape used to prevent injection via user input
@@ -8,4 +12,10 @@ app = Flask(__name__)
 # Handle all Auth API endpoint routing
 @app.route("/api/auth/<endpoint>")
 def auth_api_routing(endpoint):
+    return f'<p>Endpoint: {escape(endpoint)} is being served</p>'
+
+# Handle all Get API endpoint routing
+
+@app.route("/api/get/<endpoint>")
+def get_api_routing(endpoint):
     return f'<p>Endpoint: {escape(endpoint)} is being served</p>'
