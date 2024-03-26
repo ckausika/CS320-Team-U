9971769b69abbc79d12755dd5fc7e957ba8ab70a
Marco Diaz <marcodiazmoore@proton.me>
2024-03-26 17:15:01 -0400

    Auth.py function structure

--- backend/auth.py
+++ backend/auth.py
@@ -1,7 +1,7 @@
 # Todo:
 # 1. Connect to auth API endpoints
 #
-# 2. Find a hashing function we can import
+# 2. Find a hashing function we can import https://pypi.org/project/argon2-cffi/
 #
 # 3. Create authentication token generation function. Have DB store a
 #    hashed version of that token so they are securely stored and
@@ -27,3 +27,44 @@
 #
 # Required stored data for each account's authentication
 # [USERNAME, HASHED PASSWORD, HASHED TOKEN]
+
+import argon2 from PasswordHasher
+
+ph = passwordHasher() # Argon 2 hashing object
+
+# FUNCTION: determineHashMatch
+# Given a username, input, and data type, search for a hash
+# in the database and compare the two to see if they match.
+#
+# Input: (user: 'Username', unhashedInput: 'Unhashed password input', Type: 'Password'/'Token')
+# Output: Boolean (Success/Failure)
+#
+
+def determineHashMatch(user, unhashedInput, type)
+    if type == 'Password':
+        # Get stored password hash of user from the DB
+        #storedPasswordHash = ?
+
+        # Compare unhashed target to the stored password
+        #return ph.verify(storedPasswordHash, target)
+    elif type == 'Token':
+        # Get stored token hash of user from the DB
+        #storedTokenHash = ?
+
+        # Compare unhashed target to the stored token
+        #return ph.verify(storedTokenHash, target)
+    else:
+        return False # An unknown type was given to the function
+
+# Create an account given a username and password
+def accountCreate(username, password):
+    # Check if account with that username already exists
+    return false
+
+# Login to an account given a username and password
+def accountLogin(username, password):
+    return false
+
+# Perform an authentication request given a token
+def authConnect(token):
+    return false
