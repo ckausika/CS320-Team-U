f9d66d6b4a763fa9a43cd5b0a194794e8df1ad2e
Marco Diaz <marcodiazmoore@proton.me>
2024-03-26 14:48:00 -0400

    Converted existing APIs to return JSON

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@
 # flask --app app run
 #
 
-from flask import Flask
+from flask import Flask, jsonify
 from markupsafe import escape # escape used to prevent injection via user input
 
 app = Flask(__name__)
@@ -12,10 +12,20 @@ app = Flask(__name__)
 # Handle all Auth API endpoint routing
 @app.route("/api/auth/<endpoint>")
 def auth_api_routing(endpoint):
-    return f'<p>Endpoint: {escape(endpoint)} is being served</p>'
+    responseData = {
+        "Success": False,
+        "Endpoint used": escape(endpoint)
+    }
+
+    return jsonify(responseData)
+
 
 # Handle all Get API endpoint routing
-
 @app.route("/api/get/<endpoint>")
 def get_api_routing(endpoint):
-    return f'<p>Endpoint: {escape(endpoint)} is being served</p>'
+    responseData = {
+        "Success": False,
+        "Endpoint used": escape(endpoint)
+    }
+
+    return jsonify(responseData)
