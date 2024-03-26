30fef89fc49c8e1a989cd161840ab21e340e2dbe
attilapalabiyik <attila.palabiyik@gmail.com>
2024-03-26 17:15:13 -0400

    Material Angular and Routing
    * Added Material Angular
    * Added Material Buttons
    * Added Simple Routing

--- frontend/project/src/app/app.routes.ts
+++ frontend/project/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from '../pages/home-page/home-page.component'
+import {LoginPageComponent} from '../pages/login-page/login-page.component'
+import {PageNotFoundComponent} from '../pages/page-not-found/page-not-found.component'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home Page',
+        component: HomePageComponent
+    },
+    {
+        path: 'login',
+        title: 'Login Page',
+        component: LoginPageComponent
+    },
+    {
+        path: '**',
+        title: 'Page Not Found',
+        component: PageNotFoundComponent
+    },
+];
