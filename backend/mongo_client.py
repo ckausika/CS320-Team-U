3323bfbfa77a467ff31250547ddae132deddf606
ckausika <chandni.kausika@gmail.com>
2024-04-16 17:10:58 -0400

    Fixed error in mongo_client

--- backend/mongo_client.py
+++ backend/mongo_client.py
@@ -12,7 +12,6 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
