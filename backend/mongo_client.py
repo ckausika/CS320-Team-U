ae54fcfdb0ea3aa93f923d112be3fe8f664daee8
Michaela889 <159491140+Michaela889@users.noreply.github.com>
2024-04-16 09:08:20 -0400

    Update mongo_client.py
    Changed the login credentials from test to the new one

--- backend/mongo_client.py
+++ backend/mongo_client.py
@@ -1,6 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://test:test123@cluster0.qhjlnkv.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://dataview:dataproject123@cluster0.qhjlnkv.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
