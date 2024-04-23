4636eaa5c2f6e45d64806d149aff8c3a091dce17
Srihari Srivatsa <iamsrihari.921@gmail.com>
2024-04-23 13:45:55 -0400

    Added Application Page

---
+++ frontend/project/src/pages/application-page/application-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './application-page.component.html',
+  styleUrl: './application-page.component.css'
+})
+export class ApplicationPageComponent {
+
+}
