e71316901dcb6a533c83ce682658bfed1b58c83e
Srihari Srivatsa <iamsrihari.921@gmail.com>
2024-04-24 15:16:18 -0400

    Updated Application Page

--- frontend/project/src/pages/application-page/application-page.component.ts
+++ frontend/project/src/pages/application-page/application-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule, MatFormField} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-application-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatDividerModule,MatFormField],
   templateUrl: './application-page.component.html',
   styleUrl: './application-page.component.css'
 })
